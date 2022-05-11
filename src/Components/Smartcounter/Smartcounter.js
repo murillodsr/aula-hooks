7449df68ed21cbf655c3d041a6f5b92c0c2c9148
Murillo <murillodsr@gmail.com>
2022-05-11 18:41:33 -0300

    first commit

---
+++ src/Components/Smartcounter/Smartcounter.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+function SmartCounter() {
+  //retorna um vetor
+  //1. variavel stateful (guarda memoria)
+  //2. função amarrada a essa variavel que atualiza esse valor
+  const [quantity, upQuantity] = useState(1);
+  return (
+    <>
+      <h1>{quantity}</h1>
+      <button onClick={() => upQuantity(quantity + 1)}>Aumentar</button>
+    </>
+  )
+}
+export default SmartCounter;
